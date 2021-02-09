cf6da9497fe284467f053e8e016fca9e61b81015
余聪 <yucong@fenbi.com>
2021-02-09 15:32:00 +0800

    fix: ts define

--- index.d.ts
+++ index.d.ts
@@ -14,25 +14,26 @@ type VDom = {
 
 type NumberOrDOM = number | DOMCombine
 
-type PizzaResult = {
+type PizzaResult<R> = {
   readonly origin: React.ComponentType
   indexOf: (dom: DOMCombine) => number
   setProps: (props: any, indexOrDom: NumberOrDOM) => void
   remove: (indexOrDom: NumberOrDOM) => void
-  get: (indexOrDom: NumberOrDOM) => VDom
-  get: () => VDom[]
-  ref: (indexOrDom: NumberOrDOM) => React.Ref
-  ref: () => React.Ref[]
-  call: (methodName: String | Symbol, argvs: any[], indexOrDom: NumberOrDOM) => any
-  call: (methodName: String | Symbol, argvs: any[]) => any[]
-  call: (attrName: String | Symbol, indexOrDom: NumberOrDOM) => any
-  call: (attrName: String | Symbol) => any[]
+  get(indexOrDom: NumberOrDOM): VDom
+  get(): VDom[]
+  ref(indexOrDom: NumberOrDOM): React.Ref<R>
+  ref(): React.Ref<R>[]
+  call(methodName: String | Symbol, argvs: any[], indexOrDom: NumberOrDOM): any
+  call(methodName: String | Symbol, argvs: any[]): any[]
+  call(attrName: String | Symbol, indexOrDom: NumberOrDOM): any
+  call(attrName: String | Symbol): any[]
 }
 
-type PizzaRender<P> = ((selector: HTMLElement | NodeList | string | HTMLElement[], props: P) => PizzaResult) & {
+type PizzaRender<P, R> = ((selector: HTMLElement | NodeList | string | HTMLElement[], props: P) => PizzaResult<R>) & {
   origin: React.ComponentType
 }
 
-type Pizza = (Component: React.ComponentType) => PizzaRender
+type Pizza<P, R> = (Component: React.ComponentType<P>) => PizzaRender<P, R>
 
-export = Pizza
+declare var pizza: Pizza<any, any>
+export = pizza
